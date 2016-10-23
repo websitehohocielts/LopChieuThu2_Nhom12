16fa76c8043600c5fd23a1021ab56bdb9b32f6b8
cong196 <vancongct.96@gmail.com>
2016-10-23 21:06:25 +0700

    up css js

---
+++ js/slide.js
@@ -0,0 +1,35 @@
+<link href="fonts/glyphicons-halflings-regular.woff" rel="fonts">
+    <script type="text/javascript">
+        $(document).ready(function()
+    {
+        initSlideShow();
+        
+    });
+    function initSlideShow()
+    {
+        if($(".slideshow div").length > 1)
+        {
+            var transationTime = 2000;
+            $(".slideshow div:first").addClass('active');
+            setInterval(slideChangeImage, transationTime);
+        }
+    }
+    function slideChangeImage()
+    {
+        var active = $(".slideshow div.active");
+        if(active.length == 0)
+        {
+            active = $(".slideshow div:last");
+        }
+        
+        var next = active.next().length ? active.next() : $(".slideshow div:first");
+        active.addClass('lastactive');
+        next.css({opacity:0.0})
+                .addClass('active')
+                .animate({opacity:1.0}, 1500, function()
+                {
+                    active.removeClass("active lastactive");    
+                });
+         
+    }
+    </script>
